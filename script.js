5ce1aa05ae1ca727855886396c142439ef0f614f
TomaszSchroeder <tomasz@schroeder.city>
2019-07-20 14:29:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://evening-savannah-52706.herokuapp.com/v1/task/getTasks';
+  const trelloApiRoot = 'https://evening-savannah-52706.herokuapp.com/v1/trello/getTasks';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
