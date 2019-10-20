1f808706363f005bb3925b1f1d402c4b4de8001d
TomaszSchroeder <tomasz@schroeder.city>
2019-10-20 19:13:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
+<<<<<<< HEAD
   const apiRoot = 'https://evening-savannah-52706.herokuapp.com/v1/';
   const trelloApiRoot = 'https://evening-savannah-52706.herokuapp.com/v1/trello/';
+=======
+  const apiRoot = 'https://evening-savannah-52706.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://evening-savannah-52706.herokuapp.com/v1/trello';
+>>>>>>> parent of 0fcb7cf... Update script.js
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
